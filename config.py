06a2594fc0014fb25914d535165566e4638f78bf
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-12-23 18:48:58 +0000

    Translation Configuration

--- config.py
+++ config.py
@@ -11,3 +11,9 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+
+
+
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
