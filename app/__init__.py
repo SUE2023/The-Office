85b1b6d8f90bc9e541d175dce69180a11d793b75
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-12-23 17:12:47 +0000

    Initialization Module

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Flask application instance"""
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = _l('Please log in to access this page.')
+
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+
+from app import routes, modules
