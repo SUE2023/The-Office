217a268be9c204e66df8818604b4d08b5b8e97b4
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-12-23 18:57:51 +0000

    Babel files added

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 """ Flask application instance"""
 import logging
-from flask import Flask
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from flask import Flask, request, current_app
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_babel import Babel, lazy_gettext as _l
+from elasticsearch import Elasticsearch
 
-
+def get_locale():
+    return request.accept_languages.best_match(current_app.config['LANGUAGES'])
 
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
 login.login_view = 'auth.login'
 login.login_message = _l('Please log in to access this page.')
-
+babel = Babel()
 
 
 def create_app(config_class=Config):
@@ -21,5 +27,16 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
     db = SQLAlchemy(app)
     migrate = Migrate(app, db)
+    login.init_app(app)
+    babel.init_app(app, locale_selector=get_locale)
+    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+        if app.config['ELASTICSEARCH_URL'] else None
+
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
 from app import models
